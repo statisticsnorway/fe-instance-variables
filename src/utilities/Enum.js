115d936771b4f9dc08776e9e4601d43ad6cf2866
mnm <mnm@ssb.no>
2019-10-02 12:15:18 +0200

    Changed lds urls according to new plattform

--- src/utilities/Enum.js
+++ src/utilities/Enum.js
@@ -14,9 +14,9 @@ export const LANGUAGES = {
 
 export const LDS_URL = {
   local:  'http://localhost:9090',
-  stagingLds: 'https://lds-client.staging.ssbmod.net/be/lds',
-  stagingLdsB: 'https://lds-client.staging.ssbmod.net/be/lds-b',
-  stagingLdsC: 'https://lds-client.staging.ssbmod.net/be/lds-c'
+  stagingLds: 'https://reactapp.staging-bip-app.ssb.no/be/lds',
+  stagingLdsB: 'https://reactapp.staging-bip-app.ssb.no/be/lds-b',
+  stagingLdsC: 'https://reactapp.staging-bip-app.ssb.no/be/lds-c'
 }
 
 export const DATASTRUCTURECOMPONENTTYPE = {
