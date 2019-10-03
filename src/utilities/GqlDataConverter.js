46881c7ce7957163f3a242254b6e33bcd2e8b86f
mnm <mnm@ssb.no>
2019-10-03 09:34:56 +0200

    Allow description in edge to be undefined

--- src/utilities/GqlDataConverter.js
+++ src/utilities/GqlDataConverter.js
@@ -24,7 +24,7 @@ function mapEdge(edge) {
   return {
     id: edge.node.id,
     name: edge.node.name[0].languageText,
-    description: edge.node.description[0].languageText,
+    description: (edge.node.description) ? edge.node.description[0].languageText : '',
     title: edge.node.id
   }
 }
